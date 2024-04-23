39d99c46c3eef9a5756fe959b61585dccf324ba5
Dike Kingsley <125186879+Pure-coder007@users.noreply.github.com>
2024-04-23 20:42:14 +0100

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -1,10 +1,13 @@
 from extensions import db, migrate, cors, login_manager
 from flask import Flask
+from config import config_object
 
 
-def create_app():
+def create_app(config=config_object["development"]):
 
     app = Flask(__name__)
+
+    app.config.from_object(config)
 
     cors.init_app(app)
     login_manager.init_app(app)
