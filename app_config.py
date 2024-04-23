1490b37c32fbe228c7f041ec20f0cf8662fd9509
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-04-23 21:57:52 +0100

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -1,6 +1,7 @@
 from extensions import db, migrate, cors, login_manager
 from flask import Flask
 from config import config_object
+from endpoints import admin as admin_blueprint, school as school_blueprint
 
 
 def create_app(config=config_object["development"]):
@@ -10,8 +11,11 @@ def create_app(config=config_object["development"]):
     app.config.from_object(config)
 
     cors.init_app(app)
-    login_manager.init_app(app)
+    # login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(school_blueprint)
 
     return app
