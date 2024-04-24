a82fe2bd60b84607b3e8e5665eb1e6802b51ce4c
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-04-24 21:08:05 +0100

    Update school.py

--- endpoints/school.py
+++ endpoints/school.py
@@ -1,15 +1,34 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, session, request
 
 school = Blueprint('school', __name__)
 
 
 @school.route('/', methods=['GET'])
 def landing_page():
-    return render_template('index.html')
+    alert = session.pop('alert', None)
+    bg_color = session.pop('bg_color', None)
+    return render_template('index.html', alert=alert, bg_color=bg_color)
 
 
 @school.route('/login', methods=['GET', 'POST'])
 def login():
-    alert = "This is login"
-    bg_color = "success"
+    alert = session.pop('alert', None)
+    bg_color = session.pop('bg_color', None)
+
+    if request.method == 'POST':
+        login_code = request.form.get('login_code')
+        password = request.form.get('password')
+
+        if not login_code or not password:
+            session['alert'] = 'Please fill all the fields'
+            session['bg_color'] = 'danger'
+            return redirect(url_for('school.login'))
+
+        if login_code == 'SCHOOL' and password == 'school':
+            session['alert'] = 'Login successful'
+            session['bg_color'] = 'success'
+            return redirect(url_for('school.landing_page'))
+        session['alert'] = 'Invalid login code or password'
+        session['bg_color'] = 'danger'
+        return redirect(url_for('school.login'))
     return render_template('login.html', alert=alert, bg_color=bg_color)
