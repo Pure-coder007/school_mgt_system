1c7dbf1cd66eb62abacc809c7b86c223ae77ed12
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-04-24 16:30:16 +0100

    Update school.py

--- endpoints/school.py
+++ endpoints/school.py
@@ -6,3 +6,8 @@ school = Blueprint('school', __name__)
 @school.route('/', methods=['GET'])
 def landing_page():
     return render_template('index.html')
+
+
+@school.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
