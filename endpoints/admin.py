bd6555f83145b2eff10e5b61048816fffc5feee9
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-04-25 15:47:45 +0100

    Update admin.py

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template, redirect, url_for
 admin = Blueprint('admin', __name__)
 
 
+# ADMIN-2024-660252
+
 @admin.route('/admin', methods=['GET'])
 def get_admin():
     return render_template('index.html')
