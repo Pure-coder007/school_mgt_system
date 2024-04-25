9650c3747b9a2c5d6e559d80cba3d23dc045e758
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-04-25 15:47:37 +0100

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -2,6 +2,7 @@ from extensions import db, migrate, cors, login_manager
 from flask import Flask
 from config import config_object
 from endpoints import admin as admin_blueprint, school as school_blueprint
+from models import Admin, Course, Student, CourseRegistered
 
 
 def create_app(config=config_object["development"]):
@@ -11,9 +12,19 @@ def create_app(config=config_object["development"]):
     app.config.from_object(config)
 
     cors.init_app(app)
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+
+    with app.app_context():
+        db.create_all()
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        logged_user = Admin.query.get(user_id)
+        if not logged_user:
+            logged_user = Student.query.get(user_id)
+        return logged_user
 
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(school_blueprint)
