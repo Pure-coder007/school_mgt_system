72469ef29f1946da10a563c2b4e90b0a7e3c0a2a
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-04-26 10:07:39 +0100

    Update admin.py

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, session
+from flask_login import login_required
 
 admin = Blueprint('admin', __name__)
 
@@ -8,3 +9,29 @@ admin = Blueprint('admin', __name__)
 @admin.route('/admin', methods=['GET'])
 def get_admin():
     return render_template('index.html')
+
+
+# student_quarters
+@admin.route('/student_quarters', methods=['GET', 'POST'])
+@login_required
+def student_quarters():
+    alert = session.pop('alert', None)
+    bg_color = session.pop('bg_color', None)
+    return render_template('admin_templates/student_quarters.html', alert=alert, bg_color=bg_color, student_quarters=True)
+
+
+@admin.route('/admin_dashboard', methods=['GET'])
+@login_required
+def admin_dashboard():
+    alert = session.pop('alert', None)
+    bg_color = session.pop('bg_color', None)
+    return render_template('admin_templates/home.html', alert=alert, bg_color=bg_color, admin_dashboard=True)
+
+
+# teams
+@admin.route('/teams', methods=['GET', 'POST'])
+@login_required
+def teams():
+    alert = session.pop('alert', None)
+    bg_color = session.pop('bg_color', None)
+    return render_template('admin_templates/teams.html', alert=alert, bg_color=bg_color, teams=True)
