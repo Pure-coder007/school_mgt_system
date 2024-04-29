094a94f108b4a78b906dc2bd624719b11de2b66d
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-04-29 22:55:52 +0100

    Update admin.py

--- models/admin.py
+++ models/admin.py
@@ -4,12 +4,24 @@ from .students import code_generator
 from datetime import datetime
 from flask_login import UserMixin
 from passlib.hash import pbkdf2_sha256
+from utils import hexid
+
+
+class Role(db.Model):
+    __tablename__ = "role"
+    id = db.Column(db.String(50), default=hexid, primary_key=True, index=True)
+    name = db.Column(db.String(255))
+
+    admin = db.relationship("Admin", backref="role", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return f"Role('{self.name}')"
 
 
 class Admin(db.Model, UserMixin):
     __tablename__ = 'admin'
     # The id column is the primary key
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(50), default=hexid, primary_key=True, index=True)
     # the first_name and last_name columns are not nullable
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
@@ -20,13 +32,14 @@ class Admin(db.Model, UserMixin):
                        )
     # the email column is unique and not nullable
     email = db.Column(db.String(120), unique=True, nullable=False)
+    is_superadmin = db.Column(db.Boolean, default=False)
+    role_id = db.Column(db.String(50), db.ForeignKey('role.id'), nullable=False)
     # the password column is not nullable
     password = db.Column(db.Text, nullable=False)
 
     # The __repr__ method is used to print the object
     def __repr__(self):
         return '<Admin %r>' % self.email
-
 
 
 # This decorator is used to check if the logged-in user is an admin
@@ -43,3 +56,15 @@ class Admin(db.Model, UserMixin):
 #             abort(401, message="Admin access required")
 #         return func(*args, **kwargs)
 #     return wrapper
+
+
+roles = ["lecturer", "admin", "ict", "dean", "hod"]
+
+
+# propagate data inside the role table
+def add_roles():
+    for role in roles:
+        new_role = Role(name=role)
+        db.session.add(new_role)
+    db.session.commit()
+    return True
