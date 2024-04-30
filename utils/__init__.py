911da72d2b55ea8519fce20cec6ac7010414464a
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-04-30 10:40:24 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,12 @@
 import uuid
+import re
 
 
 def hexid():
     return uuid.uuid4().hex
+
+
+def is_valid_email(email):
+    if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return True
+    return False
