c239cb32f99c0b033ab48757c9dabe12095b9389
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-04 23:22:45 +0100

    Update admin.py

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, session, request
 from flask_login import login_required
 from models import (get_roles, Admin, get_admins,
-    get_students, get_lecturers, create_course, get_courses, Course)
+                    get_students, get_lecturers, create_course, get_courses, Course,
+                    add_student, email_exist, phone_exist)
 from passlib.hash import pbkdf2_sha256 as hasher
 from extensions import db
 from utils import is_valid_email
@@ -20,14 +21,88 @@ def get_admin():
 @admin.route('/student_quarters', methods=['GET', 'POST'])
 @login_required
 def student_quarters():
-    alert = session.pop('alert', None)
-    bg_color = session.pop('bg_color', None)
-    page = int(request.args.get('page', 1))
-    per_page = int(request.args.get('per_page', 5))
-    all_students = get_students(page, per_page)
-    return render_template('admin_templates/student_quarters.html',
-                           alert=alert, bg_color=bg_color,
-                           student_quarters=True, all_students=all_students)
+    try:
+        alert = session.pop('alert', None)
+        bg_color = session.pop('bg_color', None)
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 5))
+        all_students, total_pages, total_items = get_students(page, per_page)
+        if request.method == 'POST':
+            first_name = request.form.get('fname')
+            last_name = request.form.get('lname')
+            email = request.form.get('email')
+            phone_number = request.form.get('phone')
+            print("first_name: ", first_name, " last_name: ", last_name, " email: ", email,
+                  " phone_number: ", phone_number)
+            if not first_name or not last_name or not email or not phone_number:
+                alert = 'Please fill all the fields'
+                bg_color = 'danger'
+                return render_template('admin_templates/student_quarters.html',
+                                       alert=alert, bg_color=bg_color,
+                                       student_quarters=True, all_students=all_students,
+                                       first_name=first_name, last_name=last_name, email=email,
+                                       phone_number=phone_number, page=page, per_page=per_page,
+                                       total_pages=total_pages, total_items=total_items)
+            if not is_valid_email(email):
+                alert = 'Invalid email address'
+                bg_color = 'danger'
+                return render_template('admin_templates/student_quarters.html',
+                                       alert=alert, bg_color=bg_color,
+                                       student_quarters=True, all_students=all_students,
+                                       first_name=first_name, last_name=last_name, email=email,
+                                       phone_number=phone_number, page=page, per_page=per_page,
+                                       total_pages=total_pages, total_items=total_items)
+            if len(phone_number) != 11 or phone_number[0] != '0':
+                alert = 'Invalid phone number'
+                bg_color = 'danger'
+                return render_template('admin_templates/student_quarters.html',
+                                       alert=alert, bg_color=bg_color,
+                                       student_quarters=True, all_students=all_students,
+                                       first_name=first_name, last_name=last_name, email=email,
+                                       phone_number=phone_number, page=page, per_page=per_page,
+                                       total_pages=total_pages, total_items=total_items)
+            phone = f"+234{phone_number[1:]}"
+            if email_exist(email):
+                alert = 'Email already exist'
+                bg_color = 'danger'
+                return render_template('admin_templates/student_quarters.html',
+                                       alert=alert, bg_color=bg_color,
+                                       student_quarters=True, all_students=all_students,
+                                       first_name=first_name, last_name=last_name, email=email,
+                                       phone_number=phone_number, page=page, per_page=per_page,
+                                       total_pages=total_pages, total_items=total_items)
+            if phone_exist(phone):
+                alert = 'Phone number already exist'
+                bg_color = 'danger'
+                return render_template('admin_templates/student_quarters.html',
+                                       alert=alert, bg_color=bg_color,
+                                       student_quarters=True, all_students=all_students,
+                                       first_name=first_name, last_name=last_name, email=email,
+                                       phone_number=phone_number, page=page, per_page=per_page,
+                                       total_pages=total_pages, total_items=total_items)
+            res = add_student(first_name, last_name, email, phone)
+            if res:
+                session['alert'] = 'Student added successfully'
+                session['bg_color'] = 'success'
+                return redirect(url_for('admin.student_quarters'))
+            else:
+                alert = 'Error adding student'
+                bg_color = 'danger'
+                return render_template('admin_templates/student_quarters.html',
+                                       alert=alert, bg_color=bg_color,
+                                       student_quarters=True, all_students=all_students,
+                                       first_name=first_name, last_name=last_name, email=email,
+                                       phone_number=phone_number, page=page, per_page=per_page,
+                                       total_pages=total_pages, total_items=total_items)
+        return render_template('admin_templates/student_quarters.html',
+                               alert=alert, bg_color=bg_color,
+                               student_quarters=True, all_students=all_students,
+                               page=page, per_page=per_page, total_pages=total_pages, total_items=total_items)
+    except Exception as e:
+        print(e, "error@admin/student_quarters")
+        session['alert'] = 'NetworkError'
+        session['bg_color'] = 'danger'
+        return redirect(url_for('admin.student_quarters'))
 
 
 @admin.route('/admin_dashboard', methods=['GET'])
@@ -79,7 +154,7 @@ def teams():
                                        alert=alert, bg_color=bg_color,
                                        teams=True, roles=roles, first_name=first_name, last_name=last_name, email=email,
                                        phone_number=phone_number, selected_role=role, is_superadmin=is_superadmin,
-                                       total_pages=total_pages, total_items=total_items, teams_lists=teams_list,)
+                                       total_pages=total_pages, total_items=total_items, teams_lists=teams_list, )
 
             email_exist = Admin.query.filter_by(email=email.lower()).first()
             if email_exist:
@@ -156,7 +231,8 @@ def courses():
                                        alert=alert, bg_color=bg_color,
                                        courses=True, lecturers=lecturers, course_unit=course_unit,
                                        course_code=course_code, course_title=course_title, selected_lecturer=lecturer,
-                                       all_courses=all_courses, total_pages=total_pages, total_items=total_items, page=page, per_page=per_page)
+                                       all_courses=all_courses, total_pages=total_pages, total_items=total_items,
+                                       page=page, per_page=per_page)
 
             print("course_unit: ", course_unit, " course_code: ", course_code,
                   " course_title: ", course_title, " lecturer: ", lecturer)
