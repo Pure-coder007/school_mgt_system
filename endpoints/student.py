9667a21ef4318cbaa4b67b3e610a408935402dce
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-05 19:09:45 +0100

    Update student.py

---
+++ endpoints/student.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, redirect, url_for, session, request
+from flask_login import login_required
+
+student = Blueprint('student', __name__)
+
+
+# student dashboard
+@student.route('/student_dashboard', methods=['GET'])
+@login_required
+def student_dashboard():
+    alert = session.pop('alert', None)
+    bg_color = session.pop('bg_color', None)
+    return render_template('student_templates/home.html',
+                           student_dashboard=True, alert=alert, bg_color=bg_color)
