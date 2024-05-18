5d48c5abd26e48caaec591d926feebeb182d564e
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-18 23:59:17 +0100

    Update __init__.py

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -1,6 +1,6 @@
 # import for decorators
 from functools import wraps
-from models import Admin
+from models import Admin, Student
 from flask import request, redirect, url_for, session
 from flask_login import login_user, logout_user, current_user
 
@@ -13,8 +13,43 @@ def admin_required(func):
         # previous_endpoint = request.referrer
         # print(previous_endpoint, "previous endpoint")
         if not admin_user:
-            session['alert'] = 'You cannot access this page'
-            session['bg_color'] = 'danger'
-            return redirect(url_for('student.student_dashboard'))
+            session["alert"] = "You cannot access this page"
+            session["bg_color"] = "danger"
+            return redirect(url_for("student.student_dashboard"))
         return func(*args, **kwargs)
+
+    return wrapper
+
+
+# student required
+def student_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        student_user = Student.query.filter_by(id=current_user.id).first()
+        if not student_user:
+            session["alert"] = "You cannot access this page"
+            session["bg_color"] = "danger"
+            return redirect(url_for("admin.admin_dashboard"))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+# check if authenticated
+def check_authenticated(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if current_user.is_authenticated:
+            student_user = Student.query.filter_by(id=current_user.id).first()
+            if student_user:
+                session["alert"] = "You cannot access this page"
+                session["bg_color"] = "danger"
+                return redirect(url_for("admin.admin_dashboard"))
+            admin = Admin.query.filter_by(id=current_user.id).first()
+            if admin:
+                session["alert"] = "You cannot access this page"
+                session["bg_color"] = "danger"
+                return redirect(url_for("admin.admin_dashboard"))
+        return func(*args, **kwargs)
+
     return wrapper
