52045473090ca1053e1b696020f692d93fe42e14
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-18 23:53:11 +0100

    Update student.py

--- endpoints/student.py
+++ endpoints/student.py
@@ -3,66 +3,93 @@ from flask_login import login_required, current_user
 from passlib.hash import pbkdf2_sha256 as hasher
 from extensions import db
 
-student = Blueprint('student', __name__)
+student = Blueprint("student", __name__)
 
 
 # student dashboard
-@student.route('/student_dashboard', methods=['GET'])
+@student.route("/student_dashboard", methods=["GET"])
 @login_required
 def student_dashboard():
-    alert = session.pop('alert', None)
-    bg_color = session.pop('bg_color', None)
-    return render_template('student_templates/home.html',
-                           student_dashboard=True, alert=alert, bg_color=bg_color)
+    alert = session.pop("alert", None)
+    bg_color = session.pop("bg_color", None)
+    return render_template(
+        "student_templates/home.html",
+        student_dashboard=True,
+        alert=alert,
+        bg_color=bg_color,
+    )
 
 
-@student.route('/registered_courses', methods=['GET'])
+@student.route("/registered_courses", methods=["GET"])
 @login_required
 def registered_courses():
-    alert = session.pop('alert', None)
-    bg_color = session.pop('bg_color', None)
-    return render_template('student_templates/reg_courses.html',
-                           registered_courses=True, alert=alert, bg_color=bg_color)
+    alert = session.pop("alert", None)
+    bg_color = session.pop("bg_color", None)
+    return render_template(
+        "student_templates/reg_courses.html",
+        registered_courses=True,
+        alert=alert,
+        bg_color=bg_color,
+    )
 
 
 # edit profile
-@student.route('/edit_profile', methods=['GET', 'POST'])
+@student.route("/edit_profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    alert = session.pop('alert', None)
-    bg_color = session.pop('bg_color', None)
-    if request.method == 'POST':
-        old_password = request.form.get('old_password')
-        new_password = request.form.get('new_password')
-        confirm_password = request.form.get('confirm_password')
+    alert = session.pop("alert", None)
+    bg_color = session.pop("bg_color", None)
+    if request.method == "POST":
+        old_password = request.form.get("old_password")
+        new_password = request.form.get("new_password")
+        confirm_password = request.form.get("confirm_password")
 
         if not old_password or not new_password or not confirm_password:
-            alert = 'Please fill all the fields'
-            bg_color = 'danger'
-            return render_template('student_templates/edit_profile.html',
-                                   student_dashboard=True, alert=alert, bg_color=bg_color,
-                                   old_password=old_password, new_password=new_password,
-                                   confirm_password=confirm_password)
+            alert = "Please fill all the fields"
+            bg_color = "danger"
+            return render_template(
+                "student_templates/edit_profile.html",
+                student_dashboard=True,
+                alert=alert,
+                bg_color=bg_color,
+                old_password=old_password,
+                new_password=new_password,
+                confirm_password=confirm_password,
+            )
         if new_password != confirm_password:
-            alert = 'Passwords do not match'
-            bg_color = 'danger'
-            return render_template('student_templates/edit_profile.html',
-                                   student_dashboard=True, alert=alert, bg_color=bg_color,
-                                   old_password=old_password, new_password=new_password,
-                                   confirm_password=confirm_password)
+            alert = "Passwords do not match"
+            bg_color = "danger"
+            return render_template(
+                "student_templates/edit_profile.html",
+                student_dashboard=True,
+                alert=alert,
+                bg_color=bg_color,
+                old_password=old_password,
+                new_password=new_password,
+                confirm_password=confirm_password,
+            )
         if not hasher.verify(old_password, current_user.password):
-            alert = 'Invalid old password'
-            bg_color = 'danger'
-            return render_template('student_templates/edit_profile.html',
-                                   student_dashboard=True, alert=alert, bg_color=bg_color,
-                                   old_password=old_password, new_password=new_password,
-                                   confirm_password=confirm_password)
+            alert = "Invalid old password"
+            bg_color = "danger"
+            return render_template(
+                "student_templates/edit_profile.html",
+                student_dashboard=True,
+                alert=alert,
+                bg_color=bg_color,
+                old_password=old_password,
+                new_password=new_password,
+                confirm_password=confirm_password,
+            )
         current_user.password = hasher.hash(new_password)
         current_user.changed_password = True
         db.session.commit()
-        session['alert'] = 'Password changed successfully'
-        session['bg_color'] = 'success'
-        return redirect(url_for('student.student_dashboard'))
+        session["alert"] = "Password changed successfully"
+        session["bg_color"] = "success"
+        return redirect(url_for("student.student_dashboard"))
 
-    return render_template('student_templates/edit_profile.html',
-                           student_dashboard=True, alert=alert, bg_color=bg_color)
+    return render_template(
+        "student_templates/edit_profile.html",
+        student_dashboard=True,
+        alert=alert,
+        bg_color=bg_color,
+    )
