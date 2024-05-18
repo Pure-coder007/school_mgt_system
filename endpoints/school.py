05134b72698b8a87f1e40b01b02a8f54ae3dc6da
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-18 23:53:03 +0100

    Update school.py

--- endpoints/school.py
+++ endpoints/school.py
@@ -1,57 +1,74 @@
 from flask import Blueprint, render_template, redirect, url_for, session, request
-from models import (Admin, get_roles, Student
-                    # create_admin
-                    )
+from models import (
+    Admin,
+    get_roles,
+    Student,
+    # create_admin
+)
 from flask_login import login_required, current_user, login_user, logout_user
 from passlib.hash import pbkdf2_sha256 as hasher
 
-school = Blueprint('school', __name__)
+school = Blueprint("school", __name__)
 
 
-@school.route('/', methods=['GET'])
+@school.route("/", methods=["GET"])
 def landing_page():
     # print(get_roles())
     # print(create_admin())
-    alert = session.pop('alert', None)
-    bg_color = session.pop('bg_color', None)
-    return render_template('index.html', alert=alert, bg_color=bg_color)
+    alert = session.pop("alert", None)
+    bg_color = session.pop("bg_color", None)
+    return render_template("index.html", alert=alert, bg_color=bg_color)
 
 
-@school.route('/login', methods=['GET', 'POST'])
+@school.route("/login", methods=["GET", "POST"])
 def login():
-    alert = session.pop('alert', None)
-    bg_color = session.pop('bg_color', None)
+    alert = session.pop("alert", None)
+    bg_color = session.pop("bg_color", None)
 
-    if request.method == 'POST':
-        login_code = request.form.get('login_code')
-        password = request.form.get('password')
+    if request.method == "POST":
+        login_code = request.form.get("login_code")
+        password = request.form.get("password")
 
         if not login_code or not password:
-            alert = 'Please fill all the fields'
-            bg_color = 'danger'
-            return render_template('login.html', alert=alert, bg_color=bg_color, login_code=login_code,
-                                   password=password)
+            alert = "Please fill all the fields"
+            bg_color = "danger"
+            return render_template(
+                "login.html",
+                alert=alert,
+                bg_color=bg_color,
+                login_code=login_code,
+                password=password,
+            )
 
-        user, user_type = (Admin.query.filter_by(adm_id=login_code).first(), "admin") if login_code.startswith("ADMIN") else \
-            (Student.query.filter_by(stud_id=login_code).first(), "student")
+        user, user_type = (
+            (Admin.query.filter_by(adm_id=login_code).first(), "admin")
+            if login_code.startswith("ADMIN")
+            else (Student.query.filter_by(stud_id=login_code).first(), "student")
+        )
 
         if user and hasher.verify(password, user.password):
-            session['alert'] = 'Login successful'
-            session['bg_color'] = 'success'
+            session["alert"] = "Login successful"
+            session["bg_color"] = "success"
             login_user(user)
             if user_type == "admin":
-                return redirect(url_for('admin.admin_dashboard'))
-            return redirect(url_for('student.student_dashboard'))
-        alert = 'Invalid login code or password'
-        bg_color = 'danger'
-        return render_template('login.html', alert=alert, bg_color=bg_color, login_code=login_code, password=password)
-    return render_template('login.html', alert=alert, bg_color=bg_color)
+                return redirect(url_for("admin.admin_dashboard"))
+            return redirect(url_for("student.student_dashboard"))
+        alert = "Invalid login code or password"
+        bg_color = "danger"
+        return render_template(
+            "login.html",
+            alert=alert,
+            bg_color=bg_color,
+            login_code=login_code,
+            password=password,
+        )
+    return render_template("login.html", alert=alert, bg_color=bg_color)
 
 
-@school.route('/logout')
+@school.route("/logout")
 @login_required
 def logout():
     logout_user()
-    session['alert'] = 'Logout successful'
-    session['bg_color'] = 'success'
-    return redirect(url_for('school.landing_page'))
+    session["alert"] = "Logout successful"
+    session["bg_color"] = "success"
+    return redirect(url_for("school.landing_page"))
