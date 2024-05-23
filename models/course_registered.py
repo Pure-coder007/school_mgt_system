1cd215e81dbf0f0c71407bbbb62de06cfd1f78be
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-05-23 23:03:44 +0100

    Update course_registered.py

--- models/course_registered.py
+++ models/course_registered.py
@@ -12,18 +12,6 @@ class CourseRegistered(db.Model, UserMixin):
     score = db.Column(db.Float, nullable=False, default=0.0)
     # The grade column is not nullable with a default value of N/A
     grade = db.Column(db.String(10), default="N/A")
-    # The course_code column is not nullable, this is the course code
-    course_code = db.Column(db.String(80), nullable=False)
-    # The course_title column is not nullable, this is the course title
-    course_title = db.Column(db.String(100), nullable=False)
-    # The stud_id column is not nullable, this is the student id
-    stud_id = db.Column(db.String(80), nullable=False)
-    # The course_unit column is not nullable, this is the course unit
-    course_unit = db.Column(db.Integer, nullable=False)
-    # the first_name of the student
-    first_name = db.Column(db.String(50), nullable=False)
-    # the last_name of the student
-    last_name = db.Column(db.String(50), nullable=False)
     # the student's id, it is a foreign key linked to the students table
     student_id = db.Column(db.String(50), db.ForeignKey("students.id"), nullable=False)
     # the course's id, it is a foreign key linked to the courses table
