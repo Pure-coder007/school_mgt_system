72945c6b993739771f0427259975cf7aa4972b80
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-05-24 20:11:14 +0100

    Update courses.py

--- models/courses.py
+++ models/courses.py
@@ -7,7 +7,7 @@ from sqlalchemy import desc
 
 # This is the model for the courses
 class Course(db.Model, UserMixin):
-    __tablename__ = 'courses'
+    __tablename__ = "courses"
     # The id column is the primary key
     id = db.Column(db.String(50), default=hexid, primary_key=True, index=True)
     # the course_title column
@@ -21,16 +21,23 @@ class Course(db.Model, UserMixin):
     # the created_at column, it takes the current date as the default value
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     # the teacher column
-    teacher_id = db.Column(db.String(50), db.ForeignKey('admin.id'))
+    teacher_id = db.Column(db.String(50), db.ForeignKey("admin.id"))
     # the registered_courses column, it is a relationship with the CourseRegistered model
-    registered_courses = db.relationship('CourseRegistered', backref='course', lazy=True, cascade="all, delete")
+    registered_courses = db.relationship(
+        "CourseRegistered", backref="course", lazy=True, cascade="all, delete"
+    )
     # the student_registered column, it is also a relationship with the CourseRegistered model
     # the viewonly=True makes it a read-only relationship
     # the overlaps="course,registered_courses" makes it a read-only relationship
-    student_registered = db.relationship('CourseRegistered', viewonly=True, overlaps="course,registered_courses", backref='student_')
+    student_registered = db.relationship(
+        "CourseRegistered",
+        viewonly=True,
+        overlaps="course,registered_courses",
+        backref="student_",
+    )
 
     def __repr__(self):
-        return '<Course %r>' % self.name
+        return "<Course %r>" % self.name
 
 
 def create_course(course_title, course_code, course_unit, teacher):
@@ -38,27 +45,33 @@ def create_course(course_title, course_code, course_unit, teacher):
         course_title=course_title,
         course_code=course_code,
         course_unit=course_unit,
-        teacher_id=teacher
+        teacher_id=teacher,
     )
     db.session.add(course)
     db.session.commit()
     return course
 
 
-
 # get courses
 def get_courses(page, per_page):
-    courses = Course.query.order_by(desc(Course.created_at)).paginate(page=page, per_page=per_page, error_out=False)
+    courses = Course.query.order_by(desc(Course.created_at)).paginate(
+        page=page, per_page=per_page, error_out=False
+    )
     total_pages = courses.pages
     total_items = courses.total
 
-    return [
-        {
-            'id': course.id,
-            'course_title': course.course_title,
-            'course_code': course.course_code,
-            'course_unit': course.course_unit,
-            'created_at': course.created_at.strftime('%d-%b-%Y'),
-            "lecturer": f"{course.lecturer.last_name} {course.lecturer.first_name}"
-        } for course in courses.items
-    ], total_pages, total_items
+    return (
+        [
+            {
+                "id": course.id,
+                "course_title": course.course_title,
+                "course_code": course.course_code,
+                "course_unit": course.course_unit,
+                "created_at": course.created_at.strftime("%d-%b-%Y"),
+                "lecturer": f"{course.lecturer.last_name} {course.lecturer.first_name}",
+            }
+            for course in courses.items
+        ],
+        total_pages,
+        total_items,
+    )
