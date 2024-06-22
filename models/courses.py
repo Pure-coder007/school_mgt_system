92462358c88a70b29f67809829f88d7c5f1e717b
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-06-22 15:53:17 +0100

    Update courses.py

--- models/courses.py
+++ models/courses.py
@@ -9,21 +9,13 @@ from .course_registered import number_of_course_reg
 # This is the model for the courses
 class Course(db.Model, UserMixin):
     __tablename__ = "courses"
-    # The id column is the primary key
     id = db.Column(db.String(50), default=hexid, primary_key=True, index=True)
-    # the course_title column
     course_title = db.Column(db.String(100), nullable=False)
-    # the course_code column
     course_code = db.Column(db.String(80), unique=True, nullable=False)
-    # the year column, it takes the current year as the default value
     year = db.Column(db.Integer, nullable=False, default=datetime.now().year)
-    # the course unit column
     course_unit = db.Column(db.Integer, nullable=False)
-    # the created_at column, it takes the current date as the default value
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    # the teacher column
     teacher_id = db.Column(db.String(50), db.ForeignKey("admin.id"))
-    # the registered_courses column, it is a relationship with the CourseRegistered model
     registered_courses = db.relationship(
         "CourseRegistered", backref="course", lazy=True, cascade="all, delete"
     )
