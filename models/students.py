d45abc893266edebfe5deb6fa8c12d890f32ae6f
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-06-22 15:53:28 +0100

    Update students.py

--- models/students.py
+++ models/students.py
@@ -12,37 +12,25 @@ from .course_registered import CourseRegistered
 # function to hash the default password
 # it takes the default password as an argument
 def student_default_password(default_password):
-    # hash the default password
     pass_word = pbkdf2_sha256.hash(default_password)
-    # return the hashed password
     return pass_word
 
 
 # function to generate a unique code
 def code_generator(prefix: str):
-    # generate a random number
     unique_code = random.randint(100000, 999999)
-    # convert the random number to a string
     unique_code = str(unique_code)
-    # concatenate the prefix and the random number
-    # the prefix is an argument passed to the function
     mat_num = prefix + unique_code
-    # return the unique code
     return mat_num
 
 
 # This is the model for the students
 class Student(db.Model, UserMixin):
     __tablename__ = "students"
-    # The id column is the primary key
     id = db.Column(db.String(50), default=hexid, primary_key=True, index=True)
-    # the first_name column
     first_name = db.Column(db.String(80), nullable=False)
-    # the last_name column
     last_name = db.Column(db.String(80), nullable=False)
-    # the email column is unique and not nullable
     email = db.Column(db.String(120), unique=True, nullable=False)
-    # student phone number
     phone = db.Column(db.String(20), nullable=False)
     active = db.Column(db.Boolean, default=True)
     # student_id column is unique and not nullable, this is the student's matriculation number
@@ -53,19 +41,12 @@ class Student(db.Model, UserMixin):
         nullable=False,
         default=code_generator(f"ACA-{datetime.now().year}-"),
     )
-    # the gpa column is not nullable, the default value is 0.00
     gpa = db.Column(db.Float, nullable=False, default=0.00)
-    # the password column is not nullable, the default value is a function that hashes the default password
-    # the default password is the argument passed to the function
     password = db.Column(
         db.Text, nullable=False, default=student_default_password("academia")
     )
-    # the changed_password column is not nullable, the default value is False
-    # this column is used to check if the student has changed the default password
     changed_password = db.Column(db.Boolean, nullable=False, default=False)
-    # date
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    # the registered_courses column is a relationship with the CourseRegistered model
     registered_courses = db.relationship(
         "CourseRegistered", cascade="all, delete", backref="student", lazy=True
     )
@@ -73,20 +54,6 @@ class Student(db.Model, UserMixin):
     # The __repr__ method is used to print the object
     def __repr__(self):
         return "<User %r>" % self.email
-
-
-# This decorator is used to check if the logged-in user is a student
-# def student_required(func):
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         # Get the logged-in user
-#         logged_user = get_jwt_identity()
-#         # Check if the logged-in user is a student
-#         if not logged_user.startswith('ACA'):
-#             # If not, return an error
-#             abort(401, message="This is students arena")
-#         return func(*args, **kwargs)
-#     return wrapper
 
 
 # get all students
