aec3ff8aa9e71bbd66971739e1fac334fcd4e4d2
Adeniyi Olanrewaju <100721103+engrmarkk@users.noreply.github.com>
2024-06-22 15:52:58 +0100

    Update course_registered.py

--- models/course_registered.py
+++ models/course_registered.py
@@ -6,15 +6,10 @@ from utils import hexid
 # This is the model for the registered courses by students
 class CourseRegistered(db.Model, UserMixin):
     __tablename__ = "course_registered"
-    # The id column is the primary key
     id = db.Column(db.String(50), default=hexid, primary_key=True, index=True)
-    # The score column is not nullable with a default value of 0.0
     score = db.Column(db.Float, nullable=False, default=0.0)
-    # The grade column is not nullable with a default value of N/A
     grade = db.Column(db.String(10), default="N/A")
-    # the student's id, it is a foreign key linked to the students table
     student_id = db.Column(db.String(50), db.ForeignKey("students.id"), nullable=False)
-    # the course's id, it is a foreign key linked to the courses table
     course_id = db.Column(db.String(50), db.ForeignKey("courses.id"), nullable=False)
 
     def __repr__(self):
